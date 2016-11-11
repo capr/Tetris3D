f1156ff48747deb205c0b05bc604e6225c01a5d4
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2016-11-11 05:18:39 +0200

    unimportant

--- t3d.js
+++ t3d.js
@@ -1,15 +1,6 @@
 
-var canvas
-var cx
-
-function init_context() {
-	canvas = $('#canvas')[0]
-	cx = canvas.getContext('2d')
-}
-
-function clear_canvas() {
-	cx.clearRect(0, 0, canvas.width, canvas.height)
-}
+// 3D math -------------------------------------------------------------------
+// https://github.com/arkanis/single-header-file-c-libs/blob/master/math_3d.h
 
 function vec2(x, y) {
 	return {'x': x, 'y': y}
@@ -54,7 +45,7 @@ function m4_identity() {
 	)
 }
 
-function m4_translation(offset) {
+function m4_translated(offset) {
 	return mat4(
 		1,  0,  0,  offset.x,
 		0,  1,  0,  offset.y,
@@ -63,15 +54,20 @@ function m4_translation(offset) {
 	)
 }
 
-/**
- * Creates a matrix to rotate around an axis by a given angle. The axis doesn't
- * need to be normalized.
- *
- * Sources:
- *
- * https://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
- */
-function m4_rotation(angle_in_rad, axis) {
+function m4_scaled(scale) {
+	var x = scale.x
+	var y = scale.y
+	var z = scale.z
+	return mat4(
+		x,  0,  0,  0,
+		0,  y,  0,  0,
+		0,  0,  z,  0,
+		0,  0,  0,  1
+	)
+}
+
+// create a matrix to rotate around an axis by a given angle.
+function m4_rotated(angle_in_rad, axis) {
 	var normalized_axis = v3_norm(axis)
 	var x = normalized_axis.x
 	var y = normalized_axis.y
@@ -87,16 +83,7 @@ function m4_rotation(angle_in_rad, axis) {
 	)
 }
 
-/**
- * Multiplication of two 4x4 matrices.
- *
- * Implemented by following the row times column rule and illustrating it on a
- * whiteboard with the proper indices in mind.
- *
- * Further reading: https://en.wikipedia.org/wiki/Matrix_multiplication
- * But note that the article use the first index for rows and the second for
- * columns.
- */
+// multiply two 4x4 matrices.
 function m4_mul(a, b) {
 	var r = m4_identity()
 	for (var i = 0; i < 4; i++)
@@ -109,13 +96,7 @@ function m4_mul(a, b) {
 	return r
 }
 
-/**
- * Multiplies a 4x4 matrix with a 3D vector representing a point in 3D space.
- *
- * Before the matrix multiplication the vector is first expanded to a 4D vector
- * (x, y, z, 1). After the multiplication the vector is reduced to 3D again by
- * dividing through the 4th component (if it's not 0 or 1).
- */
+// multiply a 4x4 matrix with a 3D vector representing a point in 3D space.
 function m4_mul_p(m, p) {
 	var r = vec3(
 		m[0][0] * p.x + m[1][0] * p.y + m[2][0] * p.z + m[3][0],
@@ -128,8 +109,68 @@ function m4_mul_p(m, p) {
 	return r
 }
 
-var xoffset = 300
-var yoffset = 300
+// create an orthographic projection matrix.
+function m4_ortho(left, right, bottom, top, back, front) {
+	var l = left
+	var r = right
+	var b = bottom
+	var t = top
+	var n = front
+	var f = back
+	var tx = -(r + l) / (r - l)
+	var ty = -(t + b) / (t - b)
+	var tz = -(f + n) / (f - n)
+	return mat4(
+		2 / (r - l),  0,            0,            tx,
+		0,            2 / (t - b),  0,            ty,
+		0,            0,            2 / (f - n),  tz,
+		0,            0,            0,            1
+	)
+}
+
+// create a perspective projection matrix for a camera.
+function m4_perspective(
+	vertical_field_of_view_in_deg,
+	aspect_ratio,
+	near_view_distance,
+	far_view_distance
+) {
+	var fovy_in_rad = vertical_field_of_view_in_deg / 180 * Math.PI
+	var f = 1 / Math.tan(fovy_in_rad / 2)
+	var ar = aspect_ratio
+	var nd = near_view_distance
+	var fd = far_view_distance
+
+	return mat4(
+		f / ar,           0,                0,                0,
+		0,                f,                0,                0,
+		0,                0,               (fd+nd)/(nd-fd),  (2*fd*nd)/(nd-fd),
+		0,                0,               -1,                0
+	)
+}
+
+// build a transformation matrix for a camera that looks from `from` towards
+// `to`. `up` defines the direction that's upwards for the camera.
+function m4_look_at(from, to, up) {
+	var z = v3_muls(v3_norm(v3_sub(to, from)), -1)
+	var x = v3_norm(v3_cross(up, z))
+	var y = v3_cross(z, x)
+
+	return mat4(
+		x.x, x.y, x.z, -v3_dot(from, x),
+		y.x, y.y, y.z, -v3_dot(from, y),
+		z.x, z.y, z.z, -v3_dot(from, z),
+		0,   0,   0,    1
+	)
+}
+
+// interface -----------------------------------------------------------------
+
+var canvas
+var cx
+
+var xoffset
+var yoffset
 var scale = 100
 var d = 2
 var trans_mat = m4_identity()
@@ -141,13 +182,42 @@ function project(p) {
 	return vec2(x, y)
 }
 
-function line3(p1, p2) {
+function init_context() {
+	canvas = $('#canvas')[0]
+	canvas.width = $('#canvas').parent().width()
+	canvas.height = $('#canvas').parent().height()
+	cx = canvas.getContext('2d')
+	xoffset = canvas.width / 2
+	yoffset = canvas.height / 2
+}
+
+function clear_canvas() {
+	cx.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function line3(p1, p2, color) {
 	var p1 = project(p1)
 	var p2 = project(p2)
 	cx.beginPath()
 	cx.moveTo(p1.x, p1.y)
 	cx.lineTo(p2.x, p2.y)
+	cx.strokeStyle = color || '#ffffff'
 	cx.stroke()
+	cx.closePath()
+}
+
+function rect3(p1, p2, p3, p4, color) {
+	var p1 = project(p1)
+	var p2 = project(p2)
+	var p3 = project(p3)
+	var p4 = project(p4)
+	cx.beginPath()
+	cx.moveTo(p1.x, p1.y)
+	cx.lineTo(p2.x, p2.y)
+	cx.lineTo(p3.x, p3.y)
+	cx.lineTo(p4.x, p4.y)
+	cx.fillStyle = color || '#ffffff'
+	cx.fill()
 	cx.closePath()
 }
 
@@ -161,6 +231,13 @@ function cube() {
 	var p6 = vec3(1, 0, 1)
 	var p7 = vec3(1, 1, 1)
 	var p8 = vec3(0, 1, 1)
+
+	rect3(p1, p2, p3, p4, '#000088') // front
+	rect3(p5, p6, p7, p8, '#008800') // back
+	rect3(p1, p4, p8, p5, '#880088') // top
+	rect3(p2, p3, p7, p6, '#880000') // bottom
+	rect3(p1, p2, p6, p5, '#888800') // left
+	rect3(p3, p4, p8, p7, '#008888') // right
 
 	line3(p1, p2)
 	line3(p2, p3)
@@ -181,18 +258,24 @@ function cube() {
 $(function() {
 
 	init_context()
-	cx.strokeStyle = '#ffffff'
 
 	var a = 0
 	setInterval(function() {
-		a = a + Math.PI / 64
-		var mr = m4_rotation(a, vec3(-1, 1, 1))
-		var mt = m4_translation(vec3(-0.5, -0.5, -0.5))
-		trans_mat = m4_mul(mr, mt)
 		clear_canvas()
+
+		a = a + Math.PI / 64
+		var mr = m4_rotated(a, vec3(-1, 1, 1))
+		var mt = m4_translated(vec3(-0.5, -0.5, -0.5))
+		trans_mat = m4_mul(mr, mt)
+		cube()
+		var mt = m4_translated(vec3(1, 0, 0))
+		trans_mat = m4_mul(trans_mat, mt)
 		cube()
 
 	}, 1/30 * 1000)
 
+	$(window).resize(function() {
+		init_context()
+	})
 
 })
